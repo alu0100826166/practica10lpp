775ff0edc59e24d7971cfd08d959ab60202658a0
alu0100826166 <alu0100826166@ull.edu.es>
2015-12-10 22:19:26 +0000

    C9: Funciona APA

--- spec/spec_bibliografia.rb
+++ spec/spec_bibliografia.rb
@@ -84,7 +84,28 @@ describe Bibliografia do
             
             puts "Comienzo sort"
             puts @lista.sort
-            puts "Final sort"
+            puts "Final sort: Debe salir libro y luego revista"
+        end
+        
+        it "Ordenado segun APA" do
+            serie = "Serie1"
+            autores = Array.new
+            autores = ["Alejandro Alvaro"]
+            isbn = { "isbn-1" => " 193778", "isbn-12" => " 978-19377" }
+            @libro = Bibliografia::Libro.new(autores, "ZTituloLibro", serie, "Editorial Libro", "Numero Edicion Libro", Date.new(2015, 11, 14), isbn)
+            
+            serie2 = "Serie2"
+            autores1 = Array.new
+            autores1 = ["Alejandro Alvaro", "Florentino Fernandez"]
+            issn = "1133-9721"
+            @revista = Bibliografia::Revista.new(autores1, "TituloRevista", serie2, "Editorial Revista", "Numero Edicion Revista", Date.new(2015, 11, 15), issn)
+           
+            @lista = Bibliografia::ListaEnlazada.new(@libro)
+            @lista.insertar_delante(@revista)
+            
+            puts "Comienzo sort"
+            puts @lista.sort
+            puts "Final sort: Debe salir libro primero y luego revista"
         end
     end 
 
