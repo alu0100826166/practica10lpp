81d8916a64cdf0478174dc9ee3c8c2ffd7eb64b0
alu0100826166 <alu0100826166@ull.edu.es>
2015-12-10 22:25:07 +0000

    C10: Funciona APA

--- spec/spec_bibliografia.rb
+++ spec/spec_bibliografia.rb
@@ -82,7 +82,8 @@ describe Bibliografia do
             @lista = Bibliografia::ListaEnlazada.new(@libro)
             @lista.insertar_delante(@revista)
             
-            puts "Comienzo sort"
+            puts""
+            puts "Comienzo sort: Mismos autores exactamente, ordena por fecha"
             puts @lista.sort
             puts "Final sort: Debe salir libro y luego revista"
         end
@@ -102,10 +103,31 @@ describe Bibliografia do
            
             @lista = Bibliografia::ListaEnlazada.new(@libro)
             @lista.insertar_delante(@revista)
-            
-            puts "Comienzo sort"
+            puts""
+            puts "Comienzo sort: Mismo primer autor, imprime primero el de solo un autor"
             puts @lista.sort
             puts "Final sort: Debe salir libro primero y luego revista"
+        end
+        
+        it "Ordenado segun APA" do
+            serie = "Serie1"
+            autores = Array.new
+            autores = ["Alejandro Alvaro", "Florentino Fernandez"]
+            isbn = { "isbn-1" => " 193778", "isbn-12" => " 978-19377" }
+            @libro = Bibliografia::Libro.new(autores, "ZTituloLibro", serie, "Editorial Libro", "Numero Edicion Libro", Date.new(2015, 11, 14), isbn)
+            
+            serie2 = "Serie2"
+            autores1 = Array.new
+            autores1 = ["Alejandro Alvaro", "Florentino Fernandez"]
+            issn = "1133-9721"
+            @revista = Bibliografia::Revista.new(autores1, "TituloRevista", serie2, "Editorial Revista", "Numero Edicion Revista", Date.new(2015, 11, 14), issn)
+           
+            @lista = Bibliografia::ListaEnlazada.new(@libro)
+            @lista.insertar_delante(@revista)
+            puts""
+            puts "Comienzo sort: Mismos autores y misma fecha, ordena por titulo"
+            puts @lista.sort
+            puts "Final sort: Debe salir revista primero y luego libro"
         end
     end 
 
